d910099024a7b81613bd965413b019fe31ca0611
pearpairmai <47625384+pearmai1997@users.noreply.github.com>
2024-01-31 16:48:40 +0000

    update create table, etl load data, add data file2-5

--- 01-data-modeling-i/create_tables.py
+++ 01-data-modeling-i/create_tables.py
@@ -11,6 +11,7 @@ table_drop_actors = "DROP TABLE IF EXISTS actors"
 table_drop_repo = "DROP TABLE IF EXISTS repo"
 table_drop_payload_push = "DROP TABLE IF EXISTS payload_push"
 table_drop_commits = "DROP TABLE IF EXISTS commits"
+table_drop_org = "DROP TABLE IF EXISTS org"
 
 table_create_actors = """
     CREATE TABLE IF NOT EXISTS actors (
@@ -33,9 +34,20 @@ table_create_repo = """
     )
 """
 
+table_create_org = """
+    CREATE TABLE IF NOT EXISTS org (
+        id int,
+        login text,
+        gravatar_id text,
+        url text,
+        avatar_url text,
+        PRIMARY KEY(id)
+    )
+"""
+
 table_create_payload_push = """
     CREATE TABLE IF NOT EXISTS payload_push (
-        id int,
+        id bigint,
         size int,
         ref text,
         head text,
@@ -46,30 +58,33 @@ table_create_payload_push = """
 
 table_create_events = """
     CREATE TABLE IF NOT EXISTS events (
-        id text,
+        id bigint,
         type text,
         actor_id int,
         repo_id int,
-        payload_push_id int,
+        org_id int,
+        payload_action text,
+        payload_push_id bigint,
         public boolean,
         create_at timestamp,
         PRIMARY KEY(id),
         CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id),
         CONSTRAINT fk_repo FOREIGN KEY(repo_id) REFERENCES repo(id),
+        CONSTRAINT fk_org FOREIGN KEY(org_id) REFERENCES org(id),
         CONSTRAINT fk_payload_push FOREIGN KEY(payload_push_id) REFERENCES payload_push(id)
     )
 """
 
 table_create_commits = """
     CREATE TABLE IF NOT EXISTS commits (
-        commits_sha int,
-        author_email int,
-        author_name int,
+        commits_sha text,
+        author_email text,
+        author_name text,
         message text,
         url text,
-        push_id int,
+        payload_push_id bigint,
         PRIMARY KEY(commits_sha),
-        CONSTRAINT fk_payload_push FOREIGN KEY(push_id) REFERENCES payload_push(id)
+        CONSTRAINT fk_payload_push FOREIGN KEY(payload_push_id) REFERENCES payload_push(id)
 
     )
 """
@@ -78,6 +93,7 @@ create_table_queries = [
     table_create_actors,
     table_create_repo,
     table_create_payload_push,
+    table_create_org,
     table_create_events,
     table_create_commits
 ]
@@ -87,6 +103,7 @@ drop_table_queries = [
     table_drop_repo,
     table_drop_commits,
     table_drop_payload_push,
+    table_drop_org
 ]
 
 
--- 01-data-modeling-i/etl.py
+++ 01-data-modeling-i/etl.py
@@ -5,6 +5,48 @@ from typing import List
 
 import psycopg2
 
+actors_table_insert = ("""
+    INSERT INTO actors
+    (id, login, display_login, gravatar_id, url, avatar_url)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    ON CONFLICT (id) DO NOTHING;
+""")
+
+repo_table_insert = ("""
+    INSERT INTO repo
+    (id, name, url)
+    VALUES (%s, %s, %s)
+    ON CONFLICT (id) DO NOTHING;
+""")
+
+payload_push_table_insert = ("""
+    INSERT INTO payload_push 
+    (id, size, ref, head, before)
+ VALUES (%s, %s, %s, %s, %s)
+ ON CONFLICT (id) DO NOTHING;
+""")
+
+commits_table_insert = ("""
+    INSERT INTO commits
+    (commits_sha, author_email, author_name, message, url, payload_push_id)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    ON CONFLICT (commits_sha) DO NOTHING;
+""")
+
+org_table_insert = ("""
+    INSERT INTO org 
+    (id, login, gravatar_id, url, avatar_url)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (id) DO NOTHING;
+""")
+
+events_table_insert = ("""
+    INSERT INTO events
+    (id, type, actor_id, repo_id, payload_push_id, public, create_at)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (id) DO NOTHING;
+ """)
+
 
 def get_files(filepath: str) -> List[str]:
     """
@@ -31,56 +73,103 @@ def process(cur, conn, filepath):
         with open(datafile, "r") as f:
             data = json.loads(f.read())
             for each in data:
-                # Print some sample data
-                
-                if each["type"] == "IssueCommentEvent":
-                    print(
-                        each["id"], 
-                        each["type"],
-                        each["actor"]["id"],
-                        each["actor"]["login"],
-                        each["repo"]["id"],
-                        each["repo"]["name"],
-                        each["created_at"],
-                        each["payload"]["issue"]["url"],
-                    )
-                else:
-                    print(
-                        each["id"], 
-                        each["type"],
-                        each["actor"]["id"],
-                        each["actor"]["login"],
-                        each["repo"]["id"],
-                        each["repo"]["name"],
-                        each["created_at"],
+
+                insert_actors_val = (
+                    each["actor"]["id"], 
+                    each["actor"]["login"],
+                    each["actor"]["display_login"],
+                    each["actor"]["gravatar_id"],
+                    each["actor"]["url"],
+                    each["actor"]["avatar_url"]
                     )
 
-                # Insert data into tables here
-                insert_statement = f"""
-                    INSERT INTO actors (
-                        id,
-                        login
-                    ) VALUES ({each["actor"]["id"]}, '{each["actor"]["login"]}')
-                    ON CONFLICT (id) DO NOTHING
-                """
-                # print(insert_statement)
-                cur.execute(insert_statement)
+                insert_repo_val = (
+                    each["repo"]["id"], 
+                    each["repo"]["name"],
+                    each["repo"]["url"]
+                    )
 
-                # Insert data into tables here
-                insert_statement = f"""
+                cur.execute(actors_table_insert, insert_actors_val)
+                cur.execute(repo_table_insert, insert_repo_val)
+
+                insert_event_val = (
+                    each["id"],
+                    each["type"],
+                    each["actor"]["id"],
+                    each["repo"]["id"],
+                    each.get("payload").get("action"),
+                    each["public"],
+                    each["created_at"]
+                )
+
+                insert_events = f"""
                     INSERT INTO events (
                         id,
                         type,
-                        actor_id
-                    ) VALUES ('{each["id"]}', '{each["type"]}', '{each["actor"]["id"]}')
+                        actor_id,
+                        repo_id,
+                        payload_action,
+                        public,
+                        create_at
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s)
+
                     ON CONFLICT (id) DO NOTHING
-                """
-                # print(insert_statement)
-                cur.execute(insert_statement)
+                """   
+                cur.execute(insert_events, insert_event_val)
+                conn.commit() 
 
-                conn.commit()
+                if each.get("payload").get("push_id") != None:                
+                    insert_payload_push_val = ( 
+                        each["payload"].get("push_id"),                    
+                        each["payload"].get("size", None),
+                        each["payload"].get("ref", None),
+                        each["payload"].get("head", None),
+                        each["payload"].get("before", None),
+                    )
+                    cur.execute(payload_push_table_insert, insert_payload_push_val)
 
+                    update_events1 = f"""
+                        UPDATE events
+                        SET payload_action = 'pushed',
+                            payload_push_id = {each["payload"]["push_id"]}
+                        WHERE id = {each["id"]}
+                    """   
+                    cur.execute(update_events1)
+                    conn.commit() 
 
+                    for cmv in each["payload"]["commits"]:
+                        insert_commits_val = (
+                            cmv["sha"],
+                            cmv["author"]["email"],
+                            cmv["author"]["name"],
+                            cmv["message"],
+                            cmv["url"],
+                            each["payload"]["push_id"]
+                        )
+                        cur.execute(commits_table_insert, insert_commits_val)
+                        conn.commit() 
+
+                if each.get("org") != None:                                                                           
+                    insert_org_val = (
+                        each.get("org").get("id"),                        
+                        each.get("org").get("login"),
+                        each.get("org").get("gravatar_id"),
+                        each.get("org").get("url"),
+                        each.get("org").get("avatar_url")
+                        )    
+                    
+                    cur.execute(org_table_insert, insert_org_val)
+
+                    update_events2 = f"""
+                        UPDATE events
+                        SET org_id = {each["org"]["id"]}
+                        WHERE id = {each["id"]}
+                    """   
+
+                    cur.execute(update_events2)
+                    conn.commit()   
+
+                
 def main():
     conn = psycopg2.connect(
         "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
