52e2531f6aca5cf4ddb2bf34457b0e08b7a73787
pearpairmai <47625384+pearmai1997@users.noreply.github.com>
2024-01-31 14:55:34 +0000

    update create table file

--- 01-data-modeling-i/create_tables.py
+++ 01-data-modeling-i/create_tables.py
@@ -8,31 +8,85 @@ PostgresConn = NewType("PostgresConn", psycopg2.extensions.connection)
 
 table_drop_events = "DROP TABLE IF EXISTS events"
 table_drop_actors = "DROP TABLE IF EXISTS actors"
+table_drop_repo = "DROP TABLE IF EXISTS repo"
+table_drop_payload_push = "DROP TABLE IF EXISTS payload_push"
+table_drop_commits = "DROP TABLE IF EXISTS commits"
 
 table_create_actors = """
     CREATE TABLE IF NOT EXISTS actors (
         id int,
         login text,
+        display_login text,
+        gravatar_id text,
+        url text,
+        avatar_url text,
         PRIMARY KEY(id)
     )
 """
+
+table_create_repo = """
+    CREATE TABLE IF NOT EXISTS repo (
+        id int,
+        name text,
+        url text,
+        PRIMARY KEY(id)
+    )
+"""
+
+table_create_payload_push = """
+    CREATE TABLE IF NOT EXISTS payload_push (
+        id int,
+        size int,
+        ref text,
+        head text,
+        before text,
+        PRIMARY KEY(id)
+    )
+"""
+
 table_create_events = """
     CREATE TABLE IF NOT EXISTS events (
         id text,
         type text,
         actor_id int,
+        repo_id int,
+        payload_push_id int,
+        public boolean,
+        create_at timestamp,
         PRIMARY KEY(id),
-        CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id)
+        CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id),
+        CONSTRAINT fk_repo FOREIGN KEY(repo_id) REFERENCES repo(id),
+        CONSTRAINT fk_payload_push FOREIGN KEY(payload_push_id) REFERENCES payload_push(id)
+    )
+"""
+
+table_create_commits = """
+    CREATE TABLE IF NOT EXISTS commits (
+        commits_sha int,
+        author_email int,
+        author_name int,
+        message text,
+        url text,
+        push_id int,
+        PRIMARY KEY(commits_sha),
+        CONSTRAINT fk_payload_push FOREIGN KEY(push_id) REFERENCES payload_push(id)
+
     )
 """
 
 create_table_queries = [
     table_create_actors,
+    table_create_repo,
+    table_create_payload_push,
     table_create_events,
+    table_create_commits
 ]
 drop_table_queries = [
     table_drop_events,
     table_drop_actors,
+    table_drop_repo,
+    table_drop_commits,
+    table_drop_payload_push,
 ]
 
 
@@ -68,7 +122,7 @@ def main():
     )
     cur = conn.cursor()
 
-    #drop_tables(cur, conn)
+    drop_tables(cur, conn)
     create_tables(cur, conn)
 
     conn.close()
